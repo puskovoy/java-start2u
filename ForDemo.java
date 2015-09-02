e2468b92cbb06aa234b0ea6c9c11664017f2be9c
puskovoy <puskovoy@gmail.com>
2015-09-02 15:23:01 +0300

    работа цикла for

---
+++ ForDemo.java
@@ -0,0 +1,14 @@
+/*
+Продемонстрировать применение цикла for.
+Присвоить исходному файлу имя ForDemo.java.
+*/
+class ForDemo {
+	public static void main(String args[]) { 
+		int count;// Этот цикл выполняется пять раз.
+		
+		for(count = 0; count < 5; count = count+1)
+			
+		System.out.println("This is count: " + count);
+		System.out.println("Done!");
+}
+}
