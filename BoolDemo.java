7047ff3d2c3f13a4ba2744d275595a1b7608f8b4
puskovoy <puskovoy@gmail.com>
2015-09-03 15:49:47 +0300

    обращение с логическими значениями

---
+++ BoolDemo.java
@@ -0,0 +1,23 @@
+// Демонстрация обращения с логическими значениями, 
+class BoolDemo {
+	public static void main(String args[]) { 
+		boolean b;
+		
+		b = false;
+		System.out.println("b is " + b); 
+		
+		b = true;
+		System.out.println ("b is " + b);
+		
+		// Логическое значение можно использовать для
+		// управления условным оператором if.
+		if(b) System.out.println("This is executed.");
+		
+		b = false;
+		if(b) System.out.println("This is not executed.");
+		
+		// Логическое значение получается в результате 
+		// выполнения оператора отношения.
+		System.out.println("10 > 9 is " + (10 > 9));
+	}
+}
