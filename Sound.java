d2b48200b3066d188a57691b85b4a58c086b6e92
puskovoy <puskovoy@gmail.com>
2015-09-04 14:21:43 +0300

    расчет растояния до удара молнии

---
+++ Sound.java
@@ -0,0 +1,12 @@
+/*
+Рассчитать расстояние до удара молнии, звук от которой доходит до наблюдателя через 1,2 с.
+*/
+class Sound {
+	public static void main(String args[]) { 
+		double dist; // объявление переменной
+		
+		dist = 7.2 * 1100;
+		System.out.println("The lightning is " + dist +
+		" feet away.");
+	}
+}
