a454576169c566132fe8237d184c5fd884614bc5
puskovoy <puskovoy@gmail.com>
2015-11-13 18:18:19 +0200

    Демонстрация цикла while

---
+++ WhileDemo.java
@@ -0,0 +1,15 @@
+// Демонстрация цикла while, 
+class WhileDemo {
+	public static void main(String args[]) { 
+
+		char ch;
+		
+		// вывести буквы английского алфавита, используя цикл while 
+
+		ch = 'a';
+		while(ch <= 'z') {
+			System.out.print(ch); 
+			ch++;
+		}
+	}
+}
