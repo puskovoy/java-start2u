34d0c5e7f9eaa6c104d4ab48fb6bfe54d667d937
puskovoy <puskovoy@gmail.com>
2015-11-23 15:42:32 +0200

    Копирование массива с помощью Length

---
+++ ArrCopy.java
@@ -0,0 +1,15 @@
+class ArrCopy {
+	public static void main(String args[])	{
+		
+		int i;
+		int nums1[] = new int[10]; 
+		int nums2[] = new int[10];
+		
+		for(i=0; i < nums1.length; i++) nums1[i] = i;
+		// копировать массив nums1 в массив nums2
+		// Переменная length служит для сравнения размеров массивов.
+		if(nums2.length >= nums1.length)
+			for(i =0; i < nums2.length; i++) nums2[i] = nums1[i];
+		for(i=0; i < nums2.length; i++)	System.out.print(nums2[i] + " ");
+	}
+}
