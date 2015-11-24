f8bb14ab52676e24d6bdd2e2074672c9a1d16f00
puskovoy <puskovoy@gmail.com>
2015-11-24 14:11:17 +0200

    разновидности for-each цикла for

---
+++ ForEach.java
@@ -0,0 +1,16 @@
+// разновидности for-each цикла for. 
+class ForEach {
+	public static void main(String args[]) {
+		
+		int nums [ ] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
+		int sum = 0;
+		
+		// использовать разновидность for-each цикла for 
+		// для суммирования и отображения значений, 
+		for(int x : nums) {
+			System.out.println("Value is: " + x); 
+			sum += x;
+		}
+		System.out.println("Summation: " + sum);
+	}
+}
