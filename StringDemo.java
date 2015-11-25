2ef07378f1e4e79698b1fc82c9f0c892c7cedbe1
puskovoy <puskovoy@gmail.com>
2015-11-25 10:09:09 +0200

    Представление класса String

---
+++ StringDemo.java
@@ -0,0 +1,14 @@
+// Представление класса String, 
+class StringDemo {
+	public static void main(String args[]) {
+		
+		// объявить символьные строки разными способами 
+		String str1 = new String("Java strings are objects.");
+		String str2 = "They are constructed various ways.";
+		String str3 = new String(str2);
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
