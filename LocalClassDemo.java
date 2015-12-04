d4ab38604cfb2385ad4ed9a31899669a0db44286
puskovoy <puskovoy@gmail.com>
2015-12-04 17:28:03 +0200

    Применение класса ShowBits в качестве локального

---
+++ LocalClassDemo.java
@@ -0,0 +1,36 @@
+// Применение класса ShowBits в качестве локального, 
+class LocalClassDemo {
+	public static void main(String args[])	{
+		// Внутренний вариант класса ShowBits.
+		// Локальный класс, вложенный в метод.
+		class ShowBits {
+			int numbits;
+			ShowBits(int n) { 
+				numbits = n;
+			}
+			void show(long val) { 
+				long mask = 1;
+				// сдвинуть влево для установки единицы в нужной позиции 
+				mask <<= numbits-1;
+				int spacer = 0;
+				for (; mask != 0; mask >>>= 1){
+					if((val & mask) != 0) 
+						System.out.print("1");
+					else 
+						System.out.print("0");
+					spacer++;
+					if ((spacer % 8) == 0){
+						System.out.print(" "); 
+						spacer = 0;
+					}
+				}
+				System.out.println() ;
+			}
+		}
+		for(byte b = 0; b < 10; b++){
+			ShowBits byteval = new ShowBits(8);
+			System.out.print(b + " in binary: "); 
+			byteval.show(b);
+		}
+	}
+}
