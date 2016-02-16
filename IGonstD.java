462f69deff1994c509bf4ff4b84d7d20e21ceb5b
Андрйе Шевцов <puskovoy@gmail.com>
2016-02-16 22:44:31 +0200

    Константы в интерфейсе

---
+++ IGonstD.java
@@ -0,0 +1,20 @@
+// содержащий только константы, 
+interface IConst {
+// Константы, 
+	int MIN = 0; 
+	int MAX = 10;
+	String ERRORMSG = "Boundary Error";
+}
+class IGonstD implements IConst {
+	public static void main(String args[]) { 
+		int nums[] = new int[MAX];
+		for(int i=MIN; i < 11; i++)	{
+			if(i >= MAX) 
+				System.out.println(ERRORMSG); 
+			else {
+				nums[i] = i;
+				System.out.print(nums[i] + " ");
+			}
+		}
+	}
+} 
