1606760febe715ee243fc300a38feb93fcb5a13b
Андрйе Шевцов <puskovoy@gmail.com>
2016-02-18 20:59:54 +0200

    Применение нескольких операторов catch Exception

---
+++ ExcDemo4.java
@@ -0,0 +1,22 @@
+// Применение нескольких операторов catch.	'
+class ExcDemo4 {
+	public static void main(String args[]) {
+		// Здесь массив numer длиннее массива denom. 
+		int numer[] = { 4, 8, 16, 32, 64, 128, 256, 512 }; 
+		int denom[] = { 2, 0, 4, 4, 0, 8 };
+		for(int i=0; i<numer.length; i++){
+			try {
+				System.out.println(numer[i] + " / " + denom[i] + " is " + numer[i]/denom[i]);
+			}
+			//За блоком try следует несколько блоков catch подряд, 
+			catch (ArithmeticException exc) {
+				// перехватить исключение
+				System.out.println("Can't divide by Zero!");
+			}
+			catch (ArrayIndexOutOfBoundsException exc) {
+				// перехватить исключение
+				System.out.println("No matching element found.");
+			}
+		}
+	}
+}
