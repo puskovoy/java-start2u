99d41f16dfc49ca34bec209cbc2ce68e89f43e9b
Андрйе Шевцов <puskovoy@gmail.com>
2016-02-21 15:24:31 +0200

    Применение специально создаваемого исключения Exception

---
+++ CustomExceptDemo.java
@@ -0,0 +1,39 @@
+// Применение специально создаваемого исключения.
+// создать исключение
+class NonIntResultException extends Exception { 
+	int n; 
+	int d;
+	NonIntResultException(int i, int j) { 
+		n = i; 
+		d = j;
+	}
+	public String toString() {
+		return "Result of " + n + " / " + d +
+			" is non-integer.";
+	}
+}
+class CustomExceptDemo {
+	public static void main(String args[]) {
+		// В массиве numer содержатся нечетные числа, 
+		int numer[] = { 4, 8, 15, 32, 64, 127, 256, 512 }; 
+		int denom[] = { 2, 0, 4, 4, 0, 8 }; 
+		for (int i=0; i<numer.length; i++)	{
+			try {
+				if((numer[i]%2)	!= 0)
+				throw new NonIntResultException(numer[i], denom[i]); 
+				System.out.println(numer[i] + " / " + denom[i] + " is " + numer[i]/denom[i]);
+			}
+			catch (ArithmeticException exc) {
+				// перехватить исключение
+				System.out.println("Can't divide by Zero!");
+			}
+			catch (ArrayIndexOutOfBoundsException exc) {
+				// перехватить исключение
+				System.out.println("No matching element found.");
+			}
+			catch (NonIntResultException exc) {
+				System.out.println(exc) ;
+			}
+		}
+	}
+}
